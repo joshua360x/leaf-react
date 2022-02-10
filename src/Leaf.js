05522aac269b83fa3002d3937125ad925edbb83f
joshua360x <jkw5499@psu.edu>
2022-02-10 16:21:15 -0500

    added link to detail page

--- src/Leaf.js
+++ src/Leaf.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-export default function Leaf({ age, season, type, description, rating }) {
-  return <div>
-    <p>{type}</p>
-    <p>{season}</p>
-    <p>{description}</p>
-    <p>{age}</p>
-    <p>{rating}</p>
-  </div>;
+export default function Leaf({ id, age, season, type, description, rating }) {
+  return (
+    <Link to={`/leaves/${id}`}>
+      <div>
+        <p>{type}</p>
+        <p>{season}</p>
+        <p>{description}</p>
+        <p>{age}</p>
+        <p>{rating}</p>
+      </div>
+    </Link>
+  );
 }

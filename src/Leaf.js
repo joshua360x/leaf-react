108c362f57f1a96e112df103daea36c0a02cfee7
joshua360x <jkw5499@psu.edu>
2022-02-10 16:02:10 -0500

    rendered each leaf and added destructured objects

--- src/Leaf.js
+++ src/Leaf.js
@@ -1,5 +1,11 @@
 import React from 'react';
 
 export default function Leaf({ age, season, type, description, rating }) {
-
+  return <div>
+    <p>{type}</p>
+    <p>{season}</p>
+    <p>{description}</p>
+    <p>{age}</p>
+    <p>{rating}</p>
+  </div>;
 }

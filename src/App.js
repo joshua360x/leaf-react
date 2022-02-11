5d8201f9e56c170747102127c78cc67d2dde5167
joshua360x <jkw5499@psu.edu>
2022-02-11 14:39:13 -0500

    created Update Leaf component and set boiler code

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import { logout } from './services/fetch-utils';
 import { useState } from 'react';
 import Details from './Details';
 import NotFound from './NotFound';
+import UpdateLeaf from './UpdateLeaf';
 
 function App() {
   const [validUser, setValidUser] = useState(localStorage.getItem('supabase.auth.token'));
@@ -18,6 +19,7 @@ function App() {
           <header>
             <NavLink to="/">Home</NavLink>
             <NavLink to="/create-leaf">Create</NavLink>
+            <NavLink to="/update-leaf">Update</NavLink>
             <button onClick={logout}>Logout</button>
           </header>
         )}
@@ -33,6 +35,10 @@ function App() {
 
             <Route exact path="/create-leaf">
               {validUser ? <CreateLeaf /> : <Redirect to="/" />}
+            </Route>
+
+            <Route exact path="/update-leaf">
+              {validUser ? <UpdateLeaf /> : <Redirect to="/" />}
             </Route>
 
             <Route exact path="/leaves/:id">
---
+++ src/UpdateLeaf.js
@@ -0,0 +1,17 @@
+import React, { useEffect, useState } from 'react';
+
+export default function UpdateLeaf() {
+  const [age, setAge] = useState(1);
+  const [season, setSeason] = useState('fall');
+  const [type, setType] = useState('round');
+  const [description, setDescription] = useState('');
+  const [rating, setRating] = useState(1);
+
+  useEffect(() => {
+    
+  }, []);
+
+  return <form>
+
+  </form>;
+}

a58fe3107da209deefe595efc22aeacebca8058f
joshua360x <jkw5499@psu.edu>
2022-02-11 17:43:29 -0500

    added clean up

--- src/LeavesList.js
+++ src/LeavesList.js
@@ -5,16 +5,35 @@ import { getLeaves } from './services/fetch-utils';
 export default function LeavesList() {
   const [leaves, setLeaves] = useState([]);
 
+  // useEffect(() => {
+  //   async function onLoad() {
+  //     let controller = new AbortController();
+
+  //     const data = await getLeaves();
+  //     setLeaves(data);
+  //   }
+  //   onLoad();
+  // }, [leaves]);
+
   useEffect(() => {
-    async function onLoad() {
-      const data = await getLeaves();
-      setLeaves(data);
-    }
-    onLoad();
+    let controller = new AbortController();
+    (async () => {
+      try {
+        const response = await getLeaves();
+        setLeaves(response);
+        controller = null;
+      } catch (e) {
+        // Handle fetch error
+      }
+    })();
+    return () => controller?.abort();
   }, [leaves]);
-  return <div className='leaf-list'>
-    {
-      leaves.map((leaf, i) => <Leaf key={leaf + i} {...leaf} />)
-    }
-  </div>;
+
+  return (
+    <div className="leaf-list">
+      {leaves.map((leaf, i) => (
+        <Leaf key={leaf + i} {...leaf} />
+      ))}
+    </div>
+  );
 }
